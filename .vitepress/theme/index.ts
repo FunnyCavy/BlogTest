89cf8b7011a347a53c65b678e0204a539efb5645
Fs-DXMY <fs.dxmy@gmail.com>
2023-12-22 14:06:57 +0800

    temp

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,20 @@
+import mediumZoom from 'medium-zoom'
+import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
+import { nextTick, onMounted, watch } from 'vue'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  setup() {
+    const route = useRoute()
+    const initZoom = () => mediumZoom('.main img', {
+      background: 'var(--vp-c-bg)',
+      margin: 24
+    })
+    onMounted(() => initZoom())
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom()))
+  }
+}
